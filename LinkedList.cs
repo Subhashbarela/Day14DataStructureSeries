20f830d4a86c2929dac40e7362aeae27917bd7d0
Subhash Barela <barelasubhash16@gmail.com>
2023-02-23 16:13:23 +0530

    append the data into linked list

---
+++ LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListSeries
+{
+    internal class LinkedList
+    {
+        public Node head;
+        internal void AddLast(int last)
+        {
+            Node LastNode = new Node(last);
+
+            if (this.head == null)
+            {
+                this.head = LastNode;
+                // Console.WriteLine("list is empty ;");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = LastNode;
+            }
+         //   Console.WriteLine("{0} inserted into linklist :", LastNode.data);
+        }
+
+        internal void display()
+        {
+            Console.WriteLine("............................................\n");
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("the linked list is empty ");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            Console.WriteLine(" Insert the element at first position : ");
+            int last1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Insert the element at second lastPosition  : ");
+            int last2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Insert the element at last position : ");
+            int last = Convert.ToInt32(Console.ReadLine());
+            list.AddLast(last);
+            list.AddLast(last1);
+            list.AddLast(last2);
+            list.display();
+
+            Console.Read();
+        }
+    }
+}
