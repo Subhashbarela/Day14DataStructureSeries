a30be4bfc7f18e20cfcd64343cd895c978c131e1
Subhash Barela <barelasubhash16@gmail.com>
2023-02-23 14:45:57 +0530

    insertion Operation In LinkedList

--- Program.cs
+++ Program.cs
@@ -12,11 +12,18 @@ namespace DataStructureSeriesUsingGeneric
         {
             
           LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
+            Console.WriteLine("inser the element into linked list :");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inser the element into linked list :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inser the element into linked list :");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            list.Add(num1);
+            list.Add(num2);
+            list.Add(num3);
            
             list.display();
+            Console.Read();
         }
             
     }
