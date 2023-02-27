641302167d99586329f9e8aa6818547c34601575
Subhash Barela <barelasubhash16@gmail.com>
2023-02-27 13:02:14 +0530

    insert data at position

---
+++ LinkedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListOperation
+{
+    internal class LinkedList
+    {
+        public Node Head;
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void Print()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+
+        public void InsertAfter(int existingData, int newData)
+        {
+            Node current = Head;
+
+            while (current != null && current.Data != existingData)
+            {
+                current = current.Next;
+            }
+
+            if (current == null)
+            {
+                // The existing node with the given value was not found.
+                Console.WriteLine("Node with value " + existingData + " not found.");
+            }
+            else
+            {
+                Node newNode = new Node(newData);
+                newNode.Next = current.Next;
+                current.Next = newNode;
+            }
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            list.Print();
+            list.InsertAfter(30,40);
+
+           // list.Delete(70);
+            list.Print();
+
+
+        }
+    }
+}
